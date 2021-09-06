401011dc42c84cc467ce35ad9f6695eee6ada4a1
Christopher Wells <cwellsny@nycap.rr.com>
2021-09-06 12:03:51 -0700

    Create FlagsRegister struct

--- src/cpu.rs
+++ src/cpu.rs
@@ -23,3 +23,46 @@ impl Registers {
         self.c = (value & 0x00FF) as u8;
     }
 }
+
+const ZERO_FLAG_BYTE_POSITION: u8 = 7;
+const SUBTRACT_FLAG_BYTE_POSITION: u8 = 6;
+const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
+const CARRY_FLAG_BYTE_POSITION: u8 = 4;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct FlagsRegister {
+    pub zero: bool,
+    pub subtract: bool,
+    pub half_carry: bool,
+    pub carry: bool,
+}
+
+impl std::convert::From<FlagsRegister> for u8 {
+    fn from(flag: FlagsRegister) -> u8 {
+        let zero = if flag.zero { 1 } else { 0 };
+        let subtract = if flag.subtract { 1 } else { 0 };
+        let half_carry = if flag.half_carry { 1 } else { 0 };
+        let carry = if flag.carry { 1 } else { 0 };
+
+        zero << ZERO_FLAG_BYTE_POSITION
+            | subtract << SUBTRACT_FLAG_BYTE_POSITION
+            | half_carry << HALF_CARRY_FLAG_BYTE_POSITION
+            | carry << CARRY_FLAG_BYTE_POSITION
+    }
+}
+
+impl std::convert::From<u8> for FlagsRegister {
+    fn from(byte: u8) -> Self {
+        let zero = ((byte >> ZERO_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let subtract = ((byte >> SUBTRACT_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let half_carry = ((byte >> HALF_CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let carry = ((byte >> CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+
+        FlagsRegister {
+            zero,
+            subtract,
+            half_carry,
+            carry,
+        }
+    }
+}
--- tests/test_cpu.rs
+++ tests/test_cpu.rs
@@ -8,3 +8,35 @@ fn set_get_bc_register() {
 
     assert_eq!(23, registers.get_bc());
 }
+
+#[test]
+fn byte_to_flag_register() {
+    let byte: u8 = 0b10100000;
+
+    let expected = game_boy_emulator::cpu::FlagsRegister {
+        zero: true,
+        subtract: false,
+        half_carry: true,
+        carry: false,
+    };
+
+    let actual = game_boy_emulator::cpu::FlagsRegister::from(byte);
+
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn flag_register_to_byte() {
+    let flag_register = game_boy_emulator::cpu::FlagsRegister {
+        zero: true,
+        subtract: false,
+        half_carry: true,
+        carry: false,
+    };
+
+    let expected: u8 = 0b10100000;
+
+    let actual = u8::from(flag_register);
+
+    assert_eq!(expected, actual);
+}
